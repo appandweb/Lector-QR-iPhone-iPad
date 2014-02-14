2833c183be48ad28ff8cb2b3c1eb7c7e73ff760b
App&Web <appandweb@appandweb.es>
2014-02-14 10:27:32 +0100

    Lector QR

---
+++ LectorQR-iPhone-iPad/LectorQR-iPhone-iPad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LectorQR-iPhone-iPad
+//
+//  Created by Ruben on 14/02/14.
+//  Copyright (c) 2014 AppAndWeb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ LectorQR-iPhone-iPad/LectorQR-iPhone-iPadTests/LectorQR_iPhone_iPadTests.m
@@ -0,0 +1,34 @@
+//
+//  LectorQR_iPhone_iPadTests.m
+//  LectorQR-iPhone-iPadTests
+//
+//  Created by Ruben on 14/02/14.
+//  Copyright (c) 2014 AppAndWeb. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LectorQR_iPhone_iPadTests : XCTestCase
+
+@end
+
+@implementation LectorQR_iPhone_iPadTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
